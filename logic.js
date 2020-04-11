eccb5988bd7af362ef41e36fbdef4ad0ecb6cfd2
tomal07 <tomer.aldema@gmail.com>
2020-04-11 10:10:24 +0300

    added checking for ' in the string and made the text area inside a pre tag

--- logic.js
+++ logic.js
@@ -43,10 +43,6 @@ function snap_to_down_vertically(s) {
 
 function snap_to_left_horizontally(s, k) {
   return s;
-  // if (k == true) return s;
-  // else {
-  //   s.forEach(element => {});
-  // }
 }
 
 function snap_to_center_horizontally(s, k) {
@@ -54,7 +50,6 @@ function snap_to_center_horizontally(s, k) {
   s.forEach(element => {
     textWidth = Math.max(textWidth, element.length);
   });
-  console.log(textWidth);
 
   var new_string = [];
   var spacing;
@@ -87,40 +82,53 @@ function snap_to_right_horizontally(s, k) {
   return new_string;
 }
 
+/* The function that is called when the main button is pressed*/
 function convert() {
-  var inp = document.getElementById("in").value.split("\n");
-  var va = document.getElementById("va").value;
-  if (document.getElementById("va").disabled == true) {
-    va = "none";
+  var inp = document.getElementById("inputText").value.split("\n"); /* get the input text to convert*/
+  var foundError = false;
+  for (var i = 0; i < inp.length && !foundError; i++) { 
+    if (inp[i].includes("\'")){
+      alert("found ' in the input, Assembly doesn't support that")
+      foundError = true;
+    }
   }
-  var ha = document.getElementById("ha").value;
-  var keep = document.getElementById("keep").checked;
+  if (!foundError) {
+    var verAlign = document.getElementById("verticalAlign").value; /* get the status of vertical aligning*/
+    if (document.getElementById("verticalAlign").disabled)
+      verAlign = "none";
 
-  document.getElementById("out").value = "";
+    var horAlign = document.getElementById("horizontalAlign").value; /* get the status of horizontal aligning*/
+    if (document.getElementById("horizontalAlign").disabled)
+      verAlign = "none";
 
-  if (va == "up") inp = snap_to_up_vertically(inp);
-  else if (va == "center") inp = snap_to_center_vertically(inp);
-  else if (va == "down") inp = snap_to_down_vertically(inp);
+    var keep = document.getElementById("keep").checked; /* check the status of keeping proportions*/
 
-  if (ha == "left") inp = snap_to_left_horizontally(inp, keep);
-  else if (ha == "center") inp = snap_to_center_horizontally(inp, keep);
-  else if (ha == "right") inp = snap_to_right_horizontally(inp, keep);
+    document.getElementById("out").value = "";
 
-  for (var i = 0; i < inp.length; i++) {
-    if (i == 0)
-      document.getElementById("out").value +=
+    if (verAlign == "up") inp = snap_to_up_vertically(inp);
+    else if (verAlign == "center") inp = snap_to_center_vertically(inp);
+    else if (verAlign == "down") inp = snap_to_down_vertically(inp);
+
+    if (horAlign == "left") inp = snap_to_left_horizontally(inp, keep);
+    else if (horAlign == "center") inp = snap_to_center_horizontally(inp, keep);
+    else if (horAlign == "right") inp = snap_to_right_horizontally(inp, keep);
+
+    for (var i = 0; i < inp.length; i++) {
+      if (i == 0)
+        document.getElementById("out").value +=
         name + " db'" + inp[i] + "',10,13\n";
-    else if (i == inp.length - 1)
-      document.getElementById("out").value +=
+      else if (i == inp.length - 1)
+        document.getElementById("out").value +=
         " ".repeat(name.length) + " db'" + inp[i] + "',10,13,'$'";
-    else
-      document.getElementById("out").value +=
+      else
+        document.getElementById("out").value +=
         " ".repeat(name.length) + " db'" + inp[i] + "',10,13\n";
+    }
   }
 }
 
 function enableSelect() {
-  document.getElementById("va").disabled = !document.getElementById("va")
+  document.getElementById("verticalAlign").disabled = !document.getElementById("verticalAlign")
     .disabled;
 }
 
